4d69fbafa743e615f084837374a04487995c15e9
minji0123 <sso02027@naver.com>
2022-01-20 18:39:50 +0900

    firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import firebase from './firebase';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+console.log(firebase);
 
 ReactDOM.render(
   <React.StrictMode>
